efeb906a6de1b7a152e1566966c2eb8535b7fc30
qhalby.mulya <qhalby.mulya@gopay.co.id>
2025-03-18 12:11:49 +0700

    feat: add showBoard function

---
+++ app.js
@@ -0,0 +1,15 @@
+const pattern = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+
+showBoard(pattern);
+
+function showBoard(pattern) {
+  console.clear();
+  console.log(pattern);
+}
