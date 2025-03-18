d79d5e5e5b84dca3b8f719a2eac86ef741543bbf
qhalby.mulya <qhalby.mulya@gopay.co.id>
2025-03-18 14:17:59 +0700

    feat: run gameoflife from app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import board from "./board.js";
 import pattern from "./pattern.js";
 
-const blinkerPattern = pattern.blinker();
-board.showBoard(blinkerPattern);
+let blinkerPattern = pattern.blinker();
+
+const run = () => {
+  setInterval(() => {
+    board.showBoard(blinkerPattern);
+    blinkerPattern = board.generateNewBoard(blinkerPattern);
+  }, 200);
+};
+
+run();
